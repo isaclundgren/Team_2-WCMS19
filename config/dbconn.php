8a65be729ed2009a2a57bd7cdce1fbd563b682ae
Pontus Larsson <pontus.larsson15@gmail.com>
2020-02-20 11:37:38 +0100

    added FK

--- config/dbconn.php
+++ config/dbconn.php
@@ -12,25 +12,31 @@ try {
 
     // sql to create event_table
     $sql = "CREATE TABLE IF NOT EXISTS event_table (
-        id INT(11) UNSIGNED AUTO_INCREMENT PRIMARY KEY,
+        event_id INT(11) AUTO_INCREMENT PRIMARY KEY,
         title VARCHAR(30) NOT NULL,
         content VARCHAR(30) NOT NULL,
         category VARCHAR(50) NOT NULL,
         event_location VARCHAR(50) NOT NULL,
         event_time VARCHAR(50) NOT NULL,
-        userid int(11) NOT NULL,
+        userid int(11) DEFAULT NULL,
         reg_date TIMESTAMP DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP);
+    
     CREATE TABLE IF NOT EXISTS user_accounts (
         id INT(11) UNSIGNED AUTO_INCREMENT PRIMARY KEY,
         user_mail VARCHAR(50) NOT NULL,
         user_password VARCHAR(30) NOT NULL,
         user_admin BOOLEAN);
+    
     CREATE TABLE IF NOT EXISTS event_signup (
-        id INT(11) UNSIGNED AUTO_INCREMENT PRIMARY KEY,
-        signup_id int(11) NOT NULL,
+        signup_id INT(11) UNSIGNED AUTO_INCREMENT PRIMARY KEY,
+        event_id int(11) NOT NULL,
+        FOREIGN KEY (event_id)
+        REFERENCES event_table(event_id),
         signup_firstname VARCHAR(30) NOT NULL,
         signup_lastname VARCHAR(50) NOT NULL,
         signup_mail VARCHAR(50) NOT NULL);";
+    
+
     
         $conn->exec($sql);
        
@@ -40,7 +46,7 @@ try {
         echo $sql . "<br>" . $e->getMessage();
         }
     
-
-
+    
+     
 
 ?>
