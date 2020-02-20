005c7856e24ce8ca699560a521ee9765c643242d
Pontus Larsson <pontus.larsson15@gmail.com>
2020-02-20 13:04:39 +0100

    displaying events

---
+++ event/getevents.php
@@ -0,0 +1,10 @@
+<?
+// include('config/dbconn.php');
+
+    $sql = "SELECT * FROM event_table";
+    $stmt = $conn->prepare($sql);
+    $stmt->execute();
+    $events = $stmt->fetchAll(PDO::FETCH_ASSOC);
+
+
+    // $conn = null;?>
--- templates/header.php
+++ templates/header.php
@@ -13,4 +13,14 @@
     <link rel="stylesheet" href="css/style.css">
 </head>
 <body>
+<nav>
+    <div class="nav-wrapper">
+      <a href="#" class="brand-logo">Logo</a>
+      <ul id="nav-mobile" class="right hide-on-med-and-down">
+        <li><a href="event/createevent.php">create</a></li>
+        <li><a href="index.php">Components</a></li>
+        <li><a href="#">JavaScript</a></li>
+      </ul>
+    </div>
+  </nav>
     
