0f80cd59553a67afb30f23ba05bf16464f38a9d3
AliAlbolad <57593337+AliAlbolad@users.noreply.github.com>
2020-02-27 19:44:22 +0100

    Add files via upload

--- config/event/getevents.php
+++ config/event/getevents.php
@@ -1,21 +1,9 @@
-<?
+<?php
 // include('config/dbconn.php');
 
     $sql = "SELECT * FROM event_table";
     $stmt = $conn->prepare($sql);
     $stmt->execute();
     $events = $stmt->fetchAll(PDO::FETCH_ASSOC);
-
-
-
-    // $conn = null;?>
-
-<!-- 
-    validate user accounts
- -->
-<?
-
-
-
-
 ?>
+
